edac5116515248bd5e3c3a1bf6ac7938a05c6dcc
flyingpoptartcat@yahoo.com <flyingpoptartcat@yahoo.com@a1d4ccc1-e2bc-f62e-033a-2bf996bbcbe1>
2012-02-16 23:57:40 +0000

    version info

--- Wsorrow.pl
+++ Wsorrow.pl
@@ -17,7 +17,7 @@
 #    You should have received a copy of the GNU General Public License
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-#VERSION 1.1
+#VERSION 1.2.1
 
 use Net::Ping;
 use LWP::UserAgent;
